d6cc2c372e79e5689d790ceba566090df818ed08
Vz-3 <vondc6201@gmail.com>
2024-10-07 01:44:24 +0800

    feat: feature embed (single audio)

--- app.py
+++ app.py
@@ -5,16 +5,20 @@ import librosa
 import soundfile as sf
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
+from transformers import Wav2Vec2Processor, Wav2Vec2Model
 import logging
 import joblib 
 import datetime
 import os
+import torch
 
 models_dict = {
     "Support Vector Machine (SVM)": "./models/SVM.pkl",
     "Naive Bayes": "./models/NaiveBayes.pkl",
     "Logistic Regression": "./models/LR.pkl"
 }
+
+max_single_audio_duration = 15000 # 15 seconds
 
 logging.basicConfig(level=logging.INFO)
 
@@ -125,19 +129,132 @@ def convert_audio_format(file_path, file_name="converted audio", target_format="
         logging.error(f"Error in converting audio format: {e}")
         return None
 
+# wav2vec + ml pipeline
+class EmbedModel:
+    """ 
+    Feature representation from pre-trained SSL wav2vec model.
+
+    Args:
+        processor : wav2vec 960h base processor.
+        model : wav2vec 960h base model.
+        intermediate_embedding_layer (bool) : representation layers. Default is True.
+        layer_index (int) : specific intermediate layer to use. Default is 3.
+    """
+    def __init__(self, processor, model, intermediate_embedding_layer=True, layer_index=3, single_audio_limit = max_single_audio_duration):
+        self.processor = processor
+        self.model = model
+        self.intermediate_embedding_layer = intermediate_embedding_layer
+        self.layer_index = layer_index 
+        self.single_audio_limit = single_audio_limit # 15 seconds
+
+    def audio_representation(self, audio):
+        data, sr = sf.read(audio)
+        # Multiple channels
+        if len(data.shape) > 1 and data.shape[1] > 1:
+            print("Multiple channels, converting to mono!")
+            data = data.mean(axis=1)
+
+        float_arr = data.astype(float)
+        print(f"Audio file is converted to float tensors of shape {float_arr.shape}")
+        input_values = self.processor(float_arr, return_tensors="pt").input_values # Batch size 1
+
+        if self.intermediate_embedding_layer:
+            try:
+                hidden_state = self.model(input_values).hidden_states[self.layer_index]
+            except Exception as e:
+                print(f"-x-x-x-x-x- Check hidden layer index -x-x-x-x-x-")
+                exit(1)
+        else:
+            hidden_state = self.model(input_values).last_hidden_state
+
+        return hidden_state
+         
+    def complete_embedding(self, file_path):
+        try:
+            if os.path.isfile(file_path):
+                # Single file processing
+                audio = AudioSegment.from_file(file_path)
+                audio_duration = len(audio)
+                print(f"{file_path}: {audio_duration} seconds")
+                
+                if (audio_duration > self.single_audio_limit):
+                    raise Exception(f"Audio file is longer than max single duration of {self.single_audio_limit / 1000} seconds. Audio is around {audio_duration / 1000} seconds!")
+                
+                # Initialize DataFrame only if the audio file passes the check
+                representation_layers = self.audio_representation(file_path)
+
+                # Compute the mean of representation layers and create a row
+                representation_layers = torch.mean(representation_layers[0], dim=0)
+                row = pd.DataFrame(representation_layers.detach().numpy().reshape(1, -1), 
+                                columns=[f"feature_{i}" for i in range(self.model.config.output_hidden_size)])
+
+                return row
+            else:
+                files_in_dir = os.listdir(file_path)
+                count = 0
+
+                file_names = [file for file in files_in_dir if os.path.isfile(os.path.join(file_path, file))]
+
+                df = pd.DataFrame(columns=[f"feature_{i}" for i in range(self.model.config.output_hidden_size)])
+
+                for file_name in file_names:
+                    count +=1
+                    print(f"-x-x-x-x-x- ID{count} file is processing -x-x-x-x-x-")
+                    representation_layers = self.audio_representation(file_name)
+
+                    representation_layers = torch.mean(representation_layers[0], dim=0) 
+                    row = pd.DataFrame(representation_layers.detach().numpy().reshape(1, -1), columns=df.columns) #representation vectors stored as X
+                    df = pd.concat([df, row], ignore_index=True)
+
+                return df
+
+        except Exception as e:
+            print(f"Failed to get feature representation due to {e}")
+            return None
+
+class AudioSplit:
+    pass
+
+# Instantiation of embedModel.
+wav2vecP = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h")  #code source https://huggingface.co/transformers/v4.6.0/_modules/transformers/models/wav2vec2/modeling_wav2vec2.html,  forward function
+wav2vecM = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base-960h", output_attentions=True, output_hidden_states=True)
+
+embedModel = EmbedModel(processor=wav2vecP, model=wav2vecM)
+
 def detect(file_path, model_name, state):
-    state = state or []
-    current_time = datetime.datetime.now().strftime("%H:%M")
+    try:
+        state = state or []
+        current_time = datetime.datetime.now().strftime("%H:%M")
+        # Split files first
+        if file_path == None:
+            raise Exception("No file uploaded!")
+            
+        # Audio representation
+        df = embedModel.complete_embedding(file_path)
+        if df is None:
+            raise Exception("issue regarding file_path or audio file duration. Check logs!")
 
-    # Load and process the audio file for model prediction
-    # (Placeholder logic for model loading)
-    print(f"Using {model_name}")
-    
-    result = f"[{current_time}] {model_name}: File name is Bonafide"
-    state.append(result)
-    history = "\n".join(state)
+        df.to_csv("features.csv",index=False)
+        # Load Model & Predict
+        print(f"Using {model_name}")
+        
+        result = f"[{current_time}] {model_name}: File name is Bonafide"
+        state.append(result)
+        history = "\n".join(state)
 
-    return history, state
+        # Once done, remove the generated files.
+        os.remove("features.csv")
+        return history, state
+
+    except Exception as e:
+        logging.error(f"Error in detection audio deepfake: {e}")
+        state = state or []
+        current_time = datetime.datetime.now().strftime("%H:%M")
+        result = f"[{current_time}] Failed attempt: {e}"
+        state.append(result)
+        history = "\n".join(state)
+
+        return history, state
 
 # Interface
 with gr.Blocks(title="Audio deepfake detection UI") as demo:
@@ -174,7 +291,7 @@ with gr.Blocks(title="Audio deepfake detection UI") as demo:
                 logs_St = gr.State([])
 
                 with gr.Row():
-                    file_upload = gr.File(label="Upload Audio file")
+                    file_upload = gr.File(label=f"Upload Audio file max of {max_single_audio_duration}")
                     trim_silence_btn = gr.Button("Trim Silence", variant="primary")
                     with gr.Column():
                         trimmed_audio = gr.Audio(interactive=False)
@@ -223,10 +340,8 @@ with gr.Blocks(title="Audio deepfake detection UI") as demo:
 
             with gr.TabItem("Convert Audio format"):
                 with gr.Row():
-                    with gr.Column():
-                        with gr.Group():
-                            file_upload = gr.File(label="Upload Audio file")
-                            file_name = gr.Textbox(label="Output file name:", placeholder="converted audio")
+                    file_upload = gr.File(label="Upload Audio file")
+                            
                     with gr.Column():
                         supported_formats_radio = gr.Radio(
                             label="Format conversion options:",
@@ -236,10 +351,10 @@ with gr.Blocks(title="Audio deepfake detection UI") as demo:
                             * FLAC - high audio quality and reduced file size. <br />
                             * MP3 - smallest file size at the cost of artifacts and loss of audio quality.
                         """)
-                        convert_audio_format_btn = gr.Button("Convert Audio format", variant="primary")
+                        file_name = gr.Textbox(label="Output file name:", placeholder="converted audio")
                     converted_audio = gr.Audio(label="Converted audio",interactive=False)
                     
-
+                convert_audio_format_btn = gr.Button("Convert Audio format", variant="primary")
                 convert_audio_format_btn.click(
                     fn=convert_audio_format,
                     inputs=[file_upload, file_name, supported_formats_radio],
